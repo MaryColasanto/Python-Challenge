dc63b28fd1f3acb5622a80a76495b29ec43cd05a
MaryColasanto <colasanto.mary@gmail.com>
2021-01-01 17:53:13 -0600

    Added candidate vote counts to the candidate vote list.

--- PyPoll/PyPoll_Main.py
+++ PyPoll/PyPoll_Main.py
@@ -22,11 +22,18 @@ with open(csvpath, 'r') as csvfile:
         # Count the votes
         vote_count += 1
 
-        # Add candidates and their respective votes to separate lists
+        # Add candidates to the list and create an empty list to add candidate votes
         if row[2] not in candidates:
             candidates.append(row[2])
             candidate_votes.append(0)
 
+        # Add votes to the vote list 
+        # https://codedestine.com/python-list-index-count-example/
+        # https://www.reddit.com/r/learnpython/comments/12a452/checking_if_an_object_inside_a_list_meets_a/
+        # Identify the candidate location in the list for each row
+        candidate_index = candidates.index(row[2])
+        # Add a vote for the candidate by adding 1 in same index in the vote list
+        candidate_votes[candidate_index] +=1
 
     print("Election Results")
     print("------------------------")
