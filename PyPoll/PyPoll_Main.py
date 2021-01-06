036c987bc8de2ae620ce6f61f2f4ade3668534fe
MaryColasanto <colasanto.mary@gmail.com>
2021-01-06 14:06:26 -0600

    Replaced round function with format function to acheive the correct number of decimals for the percent votes output and modified the text file output accordingly.

--- PyPoll/PyPoll_Main.py
+++ PyPoll/PyPoll_Main.py
@@ -29,8 +29,6 @@ with open(csvpath, 'r') as csvfile:
             candidate_votes.append(0)
 
         # Add votes to the vote list 
-        # https://codedestine.com/python-list-index-count-example/
-        # https://www.reddit.com/r/learnpython/comments/12a452/checking_if_an_object_inside_a_list_meets_a/
         # Identify the candidate location in the list for each row
         candidate_index = candidates.index(row[2])
         # Add a vote for the candidate by adding 1 in same index in the vote list
@@ -43,9 +41,8 @@ with open(csvpath, 'r') as csvfile:
 
     # Calculate the percent votes for each candidate
     for x in range(len(candidate_votes)):
-        percent_votes = round((candidate_votes[x]/vote_count)*100, 3)
-        print(f"{candidates[x]}: {percent_votes}% ({candidate_votes[x]})")
-
+        percent_votes = (candidate_votes[x]/vote_count)*100
+        print(str(candidates[x]) + ": " + '{:.3f}%'.format(percent_votes) + " " + "(" + str(candidate_votes[x]) + ")")
         # Determine the winner
         if winner_votes < candidate_votes[x]:
             winner_votes = candidate_votes[x]
@@ -63,10 +60,10 @@ file.write("Election Results")
 file.write("\n------------------------")
 file.write(f"\nTotal Votes: {vote_count}")
 file.write("\n------------------------")
-file.write(f"\n{candidates[0]}: {round((candidate_votes[0]/vote_count)*100, 3)}% ({candidate_votes[0]})")
-file.write(f"\n{candidates[0]}: {round((candidate_votes[1]/vote_count)*100, 3)}% ({candidate_votes[1]})")
-file.write(f"\n{candidates[0]}: {round((candidate_votes[2]/vote_count)*100, 3)}% ({candidate_votes[2]})")
-file.write(f"\n{candidates[0]}: {round((candidate_votes[3]/vote_count)*100, 3)}% ({candidate_votes[3]})")
+file.write("\n" + (candidates[0]) + ": " + '{:.3f}%'.format((candidate_votes[0]/vote_count)*100) + " " + "(" + str(candidate_votes[0]) + ")")
+file.write("\n" + (candidates[1]) + ": " + '{:.3f}%'.format((candidate_votes[1]/vote_count)*100) + " " + "(" + str(candidate_votes[1]) + ")")
+file.write("\n" + (candidates[2]) + ": " + '{:.3f}%'.format((candidate_votes[2]/vote_count)*100) + " " + "(" + str(candidate_votes[2]) + ")")
+file.write("\n" + (candidates[3]) + ": " + '{:.3f}%'.format((candidate_votes[3]/vote_count)*100) + " " + "(" + str(candidate_votes[3]) + ")")
 file.write("\n------------------------")
 file.write(f"\nWinner: {winner}")
 file.write("\n------------------------")
